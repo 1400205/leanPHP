c77e439d6b3d3cc89ecd08ecac0771e39c7a87ba
1400205 <1400205@rgu.ac.uk>
2016-03-06 10:36:45 +0000

    Initial commit

---
+++ learnPHP.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: prosper
+ * Date: 06/03/2016
+ * Time: 10:30
+ */
+
+//Declare age variable
+$age=1;
+ while ($age<=17)
+ {
+     print "you are under 18 and your age is $age";
+
+     $age++;
+ }
+
+
+?>
